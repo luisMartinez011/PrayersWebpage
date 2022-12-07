8e060edf7119f1f4ef125cc625e8571c398ba40e
Your Name <luis.martinez33@protonmail.com>
2022-12-07 15:00:22 -0600

    bug de inicio de sesion arreglado

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,10 @@ import { Categories } from './categories/Categories';
 import Login from '../components/Login/Login';
 
 export const Header = () => {
-  let registro = 0;
-  if (registro == 1) {
+
+  let getUser = JSON.parse(localStorage.getItem('user-info'))
+  if (getUser) {
+
     return (
       <Navbar bg="success" expand="lg" className='navbar-black  bg-success  navbar'>
         <Container>
@@ -36,9 +38,10 @@ export const Header = () => {
               <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514ZM11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
               <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
             </svg></NavLink>
+            <NavLink className='menu-link text-white ' style={{ fontSize: "2rem" }}>{getUser.nombre}</NavLink>
           </Navbar.Collapse>
         </Container>
-      </Navbar>
+      </Navbar >
     )
   }
   else {
@@ -67,6 +70,7 @@ export const Header = () => {
               <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" />
               <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z" />
             </svg></NavLink>
+
           </Navbar.Collapse>
         </Container>
       </Navbar>
