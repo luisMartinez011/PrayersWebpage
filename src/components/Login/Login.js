ee7b6f3bd18d7ee8d4bd30c41a5da5ffd1ebd49f
¨BrianDorantes¨ <brian_dorantes@icloud.com>
2023-03-29 00:55:39 -0600

    Se mejora loggin y registro

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,6 @@ import { useNavigate } from 'react-router-dom'
 import '../../../src/assets/css/Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react'
-import Header from '../Header';
 function Login() {
   let registro;
   const [email, setemail] = useState("");
@@ -37,25 +36,37 @@ function Login() {
     history("/Registro")
   }
   return (
-    <div className="containerPrincipal">
-      <div className="containerSecundario">
-        <h1>Ingrese Su Usuario Para Ingresar</h1>
-        <div className="form-group">
-          <label> usuario: </label>
-          <br />
-          <input type="text" className="form-control" onChange={(e) => setemail(e.target.value)} />
-          <br />
-          <label>Contraseña: </label>
-          <br />
-          <input type="password" className="form-control" onChange={(e) => setpassword(e.target.value)} />
-          <br />
-          <button className="btn btn-primary" onClick={Logueo} registro={registro} >Iniciar Sesion </button>
-          <br />
-          <br />
-          <button className="btn btn-primary" onClick={Registro} >Crear Cuenta </button>
+      <div className="align">
+        <div className="grid align__item">
+          <div className="register">
+            <svg xmlns="http://www.w3.org/2000/svg" className="site__logo" width="56" height="84"
+                 viewBox="77.7 214.9 274.7 412">
+              <defs>
+                <linearGradient id="a" x1="0%" y1="0%" y2="0%">
+                  <stop offset="0%" stop-color="#8ceabb"/>
+                  <stop offset="100%" stop-color="#378f7b"/>
+                </linearGradient>
+              </defs>
+              <path fill="url(#a)"
+                    d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/>
+            </svg>
+            <h2>Iniciar Sesion</h2>
+            <div className="form">
+              <div className="form__field">
+            <label>Usuario:</label>
+            <input type="email" placeholder="info@mailaddress.com" onChange={(e) => setemail(e.target.value)} />
+              </div>
+              <div className="form__field">
+            <label>Contraseña: </label>
+            <input type="password" placeholder="••••••••••••" onChange={(e) => setpassword(e.target.value)} />
+              </div>
+            <button className="btn-primary" onClick={Logueo} registro={registro} >Iniciar Sesion </button>
+            </div>
+            <p>No tienes cuenta? <a onClick ={Registro}> Crear Cuenta</a></p>
         </div>
       </div>
-    </div>
-  )
+      </div>
+
+)
 }
 export default Login
--- src/components/Registrarse/Registrarse.js
+++ src/components/Registrarse/Registrarse.js
@@ -24,27 +24,39 @@ function Registro()
         history("/")
     }
     return(
-        <div className="containerPrincipal">
-        <div className="containerSecundario">
-        <h1>Ingrese Su Usuario Para Registrarse</h1>
-        <div className="form-group">
-        <label> correo: </label>
-        <br/>
-        <input type="text" value = {email} className="form-control" onChange={(e) => setemail(e.target.value)} />
-        <br/>
-        <label>Contraseña: </label>
-        <br/>
-        <input type="password" value = {password} className="form-control" onChange={(e) => setpassword(e.target.value)} />
-        <br/>
-        <label> nombre: </label>
-        <br/>
-        <input type="text" value = {name} className="form-control" onChange={(e) => setname(e.target.value)} />
-        <br/>
-        <br/>
-        <button className="btn btn-primary" onClick={() => SignUp()}>Registrarse </button>
+        <div className="align">
+            <div className="grid align__item">
+                <div className="register">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="site__logo" width="56" height="84"
+                         viewBox="77.7 214.9 274.7 412">
+                        <defs>
+                            <linearGradient id="a" x1="0%" y1="0%" y2="0%">
+                                <stop offset="0%" stop-color="#8ceabb"/>
+                                <stop offset="100%" stop-color="#378f7b"/>
+                            </linearGradient>
+                        </defs>
+                        <path fill="url(#a)"
+                              d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/>
+                    </svg>
+                    <h2>Registro</h2>
+                    <div className="form">
+                        <div className="form__field">
+                            <label>Nombre:</label>
+                            <input type="text" placeholder="Juan Perez" value = {name} className="form-control" onChange={(e) => setname(e.target.value)} />
+                        </div>
+                        <div className="form__field">
+                            <label>Correo: </label>
+                            <input type="email" placeholder="info@mailaddress.com" value = {email} className="form-control" onChange={(e) => setemail(e.target.value)} />
+                        </div>
+                        <div className="form__field">
+                            <label>Contraseña: </label>
+                            <input type="password" placeholder="••••••••••••" value = {password} className="form-control" onChange={(e) => setpassword(e.target.value)} />
+                        </div>
+                        <button className="btn-primary" onClick={() => SignUp()}>Registrarse</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-      </div>
     )
 }
 export default Registro
